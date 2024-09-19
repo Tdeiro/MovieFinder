08c82645c9fcce9ed86cb2e045359f76f3c2c30a
Tdeiro <thdeiro@gmail.com>
2024-09-19 19:12:31 +1000

    Create http client

--- MovieFinder/src/App.tsx
+++ MovieFinder/src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieCard from "./components/MovieCard";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_KEY}`
+      );
+      setMovies(response.data.results);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(movies);
+  }, [movies]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello</h1>
+        {movies
+          ? movies.map((movie: MovieType) => (
+              <MovieCard
+                key={movie.id}
+                title={movie.title}
+                popularity={movie.popularity}
+                image={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              />
+            ))
+          : []}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+type MovieType = {
+  title: string;
+  popularity: number;
+  backdrop_path: string;
+  id: number;
+};
