4806e13e56f3604f3b874db3a5c7c6417bc3f543
Tdeiro <thdeiro@gmail.com>
2024-09-25 19:01:16 +1000

    Add movie card

--- src/components/share/MovieCard.tsx
+++ src/components/share/MovieCard.tsx
@@ -1,12 +1,31 @@
 import { BaseMovie } from "./types";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const MovieCard = (props: BaseMovie) => {
   return (
-    <div key={props.id}>
-      <img src={props.backdrop_path} style={{ maxWidth: "200px" }}></img>
-      <p>{props.title}</p>
-      <p>{props.popularity}</p>
-      <button>Add to watchlist</button>
+    <div style={{ display: "flex" }}>
+      <Card style={{ backgroundColor: "grey" }}>
+        <div style={{ alignContent: "center" }}>
+          <CardContent key={props.id}>
+            <img style={{ maxWidth: "200px" }} src={props.backdrop_path} />
+            <Typography variant="h5" component="div">
+              {props.title}
+            </Typography>
+            <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
+              <span>Rating:</span> {props.popularity}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Add to watchlist</Button>
+          </CardActions>
+        </div>
+      </Card>
     </div>
   );
 };
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Slider from "react-slick";
 import MovieCard from "../components/share/MovieCard";
 import { BaseMovie } from "../components/share/types";
 import { getMovies } from "../getMovies";
@@ -7,20 +9,29 @@ export const MainPage = () => {
 
   return moviesMapper(popularMovies);
 };
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+};
 
 const moviesMapper = (movies: any) => {
   return (
     <div className="main-page">
-      {movies
-        ? movies.map((movie: BaseMovie) => (
-            <MovieCard
-              key={movie.id}
-              title={movie.title}
-              popularity={movie.popularity}
-              backdrop_path={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            />
-          ))
-        : []}
+      <Slider {...settings}>
+        {movies
+          ? movies.map((movie: BaseMovie) => (
+              <MovieCard
+                key={movie.id}
+                title={movie.title}
+                popularity={movie.popularity}
+                backdrop_path={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              />
+            ))
+          : []}
+      </Slider>
     </div>
   );
 };
